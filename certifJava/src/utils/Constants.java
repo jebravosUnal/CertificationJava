4cdf050fc96903e15f9a32fee64eaf947d19ccac
Esteban Bravo <esteban.bravo@viseo.com>
2017-07-11 14:05:05 +0200

    chapter6

---
+++ certifJava/src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by EBR3556 on 11/07/2017.
+ */
+public class Constants {
+
+    public final static String ESTEBAN_PATH = "C:\\Users\\ebr3556\\Desktop\\Esteban\\chapter6\\";
+
+}
